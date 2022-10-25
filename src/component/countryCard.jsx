2dc628f520d9c171c6619cd949e841ee52b30f48
shakthi <shakthikumar123456789@gmail.com>
2022-10-25 23:13:11 +0530

    Create country card component

---
+++ src/component/countryCard.jsx
@@ -0,0 +1,25 @@
+const Card = ({ country }) => {
+  return (
+    <div className="card">
+      <div className="country-img">
+        <img src={country.img} alt={`${country.name} country flag`} />
+      </div>
+
+      <div className="details">
+        <div className="country-name">{country.name}</div>
+
+        <div className="population">
+          population: <output>{country.population}</output>
+        </div>
+        <div className="region">
+          region: <output>{country.region}</output>
+        </div>
+        <div className="capital">
+          capital: <output>{country.capital}</output>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
