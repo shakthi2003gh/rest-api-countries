d3aa31ca8d62273322ff4a6b95f1f16131e1c20a
shakthi <shakthikumar123456789@gmail.com>
2022-10-26 16:19:34 +0530

    Create search input

---
+++ src/component/searchBar.jsx
@@ -0,0 +1,10 @@
+const Searchbar = () => {
+  return (
+    <div className="search-bar">
+      <span className="material-symbols-outlined">search</span>
+      <input type="text" placeholder="Search for a country..." />
+    </div>
+  );
+};
+
+export default Searchbar;
